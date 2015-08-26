ad3355853d6ad48f63e53539414c3856a08aa53a
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-08-26 07:00:17 +0200

    Initial Valid Solution

---
+++ Euler2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace Euler2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint i = 1;
+            uint j = 2;
+            long s = 2;
+            while (j < 4000000)
+            {
+                uint t = i + j;
+                i = j;
+                j = t;
+                if (t % 2 == 0)
+                    s += t;
+            }
+            Console.WriteLine(s);
+            Console.ReadLine();
+        }
+    }
+}
